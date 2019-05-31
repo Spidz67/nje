1062f4aa9f2229e54592e3bbb528e5e1bf1c0c97
Carlton Mpofu <G17M7744@ict.ru.ac.za>
2019-05-31 14:34:09 +0200

    Programs runs properly

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public string Prefix(string input){
+            if(input == null){
+                return null;
+            }
+            string[] myStrings = input.Split();
+            int count = 0;
+            foreach(string word in myStrings){
+                string current = word.Replace(" ","");
+                if(current.Length != 0){
+                    count++;
+                }
+            }
+            return $"{input.Length},{count}:{input}";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            Console.WriteLine(p.Prefix("what ... did you say?? "));
         }
     }
 }
