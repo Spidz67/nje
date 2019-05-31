27d84a44c6d237fa552eae4d0eb7ff1e6999a7ab
Carlton Mpofu <G17M7744@ict.ru.ac.za>
2019-05-31 16:11:22 +0200

    done

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -15,23 +15,13 @@ namespace Tests
         {
             Program p = new Program();
             string result = p.Prefix("hello");
+            string result1 = p.Prefix("");
+            string result2 = p.Prefix("what ... did you say?? ");
             Assert.AreEqual(result,"5,1:hello");
+            Assert.AreEqual(result1,"0,0:");
+            Assert.AreEqual(result2,"23,5:what ... did you say?? ");
+
         }
 
-        [Test]
-        public void Test2()
-        {
-            Program p = new Program();
-            string result = p.Prefix("");
-            Assert.AreEqual(result,"0,0:");
-        }
-
-        [Test]
-        public void Test3()
-        {
-            Program p = new Program();
-            string result = p.Prefix("what ... did you say?? ");
-            Assert.AreEqual(result,"23,5:what ... did you say?? ");
-        }
     }
 }
